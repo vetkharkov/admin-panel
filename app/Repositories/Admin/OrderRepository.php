f6ad54bde85afa0b7feac9cbd2aaa40c2898e4a2
vetkharkov <bvpkharkov@gmail.com>
2020-02-22 00:17:58 +0200

    Create MainRepository, OrderRepository, ProductRepository. Add widgets for admin-panel.

---
+++ app/Repositories/Admin/OrderRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+
+use App\Repositories\CoreRepository;
+use App\Models\Admin\Order as Model;
+
+class OrderRepository extends CoreRepository
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    public function getAllOrders($perpage)
+    {
+        $orders = $this->startConditions()::withTrashed()
+            ->join('users', 'orders.user_id', '=', 'users.id')
+            ->join('order_products', 'order_products.order_id', '=', 'orders.id')
+            ->select('orders.id', 'orders.user_id', 'orders.status',
+                'orders.created_at', 'orders.updated_at', 'orders.currency', 'users.name',
+                \DB::raw('ROUND(SUM(order_products.price), 2) AS sum'))
+            ->groupBy('orders.id')
+            ->orderBy('orders.status')
+            ->orderBy('orders.id')
+            ->toBase()
+            ->paginate($perpage);
+
+        return $orders;
+    }
+
+
+}
